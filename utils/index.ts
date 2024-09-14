2e863dffca3ade59b4d2a6d89d7bd46c3eedd698
goncanegis <gonca.negis@gmail.com>
2024-09-14 22:28:28 +0300

    refactor horse utility functions: rename and enhance color generation, add race scheduling and performance calculation

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
-import type { Horse } from "../types";
+import type { Horse, RaceRun } from "../types";
 import {
-  STABLE,
+  HORSE_COLORS,
   MIN_CONDITION,
   MAX_CONDITION,
   JOCKEY_SILKS,
@@ -8,6 +8,8 @@ import {
   NUM_RUNNING_HORSES,
   LEN_RUNS,
 } from "~/data/constants";
+
+type ColorArray = string[] | string[][];
 
 /**
  * Helper functions to create initial horses
@@ -48,18 +50,27 @@ export const generateUniqueCondition = (
   return condition;
 };
 
-export const generateUniqueColors = (
+export const generateUniqueSilks = (
   silkArr: string[][],
   createdColors: string[][]
 ): string[] => {
   const colors = silkArr[Math.floor(Math.random() * silkArr.length)];
   for (const colorPair of createdColors) {
     if (colorPair[0] === colors[0] && colorPair[1] === colors[1]) {
-      return generateUniqueColors(silkArr, createdColors);
+      return generateUniqueSilks(silkArr, createdColors);
     }
   }
   createdColors.push(colors);
   return colors;
+};
+
+export const generateUniqueHorseColor = (createdColors: string[]): string => {
+  const color = HORSE_COLORS[Math.floor(Math.random() * HORSE_COLORS.length)];
+  if (createdColors.includes(color)) {
+    return generateUniqueHorseColor(createdColors);
+  }
+  createdColors.push(color);
+  return color;
 };
 
 /**
@@ -92,9 +103,12 @@ export const createInitialHorses = (
 
   const silks = cleanData(JOCKEY_SILKS);
 
+  const colors = cleanData(HORSE_COLORS);
+
   const usedNameIndexes: number[] = [];
   const usedConditions: number[] = [];
-  const usedColors: string[][] = [];
+  const usedSilkColors: string[][] = [];
+  const usedHorseColors: string[] = [];
 
   const horses: Horse[] = [];
 
@@ -103,39 +117,107 @@ export const createInitialHorses = (
 
     const condition = generateUniqueCondition(usedConditions);
 
-    const colors = generateUniqueColors(silks, usedColors);
+    const silkColors = generateUniqueSilks(silks, usedSilkColors);
+
+    const color = generateUniqueHorseColor(usedHorseColors);
 
     horses.push({
       name,
       id: i,
-      color: colors,
+      jockeySilk: silkColors,
       condition,
+      color,
     });
 
     usedNameIndexes.push(index);
     usedConditions.push(condition);
-    usedColors.push(colors);
+    usedSilkColors.push(colors);
+    usedHorseColors.push(color);
   }
 
   return horses;
 };
 
 /**
+ * --------------
+ * Racing Schedule
+ * --------------
+ *  */
+
+/**
  * Function that will generate a racing schedule of 6 runs with 10 random horses each.
  * Horses will be randomly picked from the initial horses array.
  * The generated array will be an array of objects containing horses and the length of the run.
  */
-export const generateRaceSchedule = (
-  horses: Horse[]
-): { horses: Horse[]; length: number }[] => {
+export const generateRaceSchedule = (horses: Horse[]): RaceRun[] => {
   const schedule: { horses: Horse[]; length: number }[] = [];
+  const availableHorses = [...horses]; // Create a copy of the horses array to keep track of available horses
+
   for (let i = 0; i < NUM_RUNS; i++) {
     const run: Horse[] = [];
+    const usedIndices = new Set<number>(); // Keep track of used indices for the current run
+
     for (let j = 0; j < NUM_RUNNING_HORSES; j++) {
-      const index = Math.floor(Math.random() * horses.length);
-      run.push(horses[index]);
+      let index;
+      do {
+        index = Math.floor(Math.random() * availableHorses.length);
+      } while (usedIndices.has(index)); // Ensure the horse is unique for the current run
+
+      usedIndices.add(index);
+      run.push(availableHorses[index]);
     }
+
     schedule.push({ horses: run, length: LEN_RUNS[i] });
   }
+
   return schedule;
+};
+
+/**
+ * --------------
+ * Races and Results
+ * --------------
+ *  */
+
+/**
+ * Helper function to wait for a specified amount of time
+ */
+export const wait = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Start a race of 6 runs and return the results
+ */
+export const runRace = async (race: RaceRun) => {
+  // Calculate the results by using horses' condition and race length
+  const raceResults = race.horses.map((horse) => {
+    const performance = horse.condition / race.length;
+    const animationSpeed = calculateAnimationSpeed(horse.condition);
+    return { horse, performance, animationSpeed };
+  });
+
+  // Sort the results by performance in descending order
+  raceResults.sort((a, b) => b.performance - a.performance);
+
+  return raceResults;
+};
+
+export const calculateAnimationSpeed = (condition: number) => {
+  if (condition <= 50) {
+    return 0.75;
+  }
+
+  if (condition > 50 && condition <= 75) {
+    return 1;
+  }
+
+  return 1.5;
+};
+
+export const calculatePerformance = (condition: number, allHorses: Horse[]) => {
+  const bestCondition = Math.max(...allHorses.map((horse) => horse.condition));
+
+  const performance = (condition / bestCondition) * 100;
+
+  return performance;
 };
