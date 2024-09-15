4e7994ed2a4b76807d0807e85d7a56e7d01607ad
goncanegis <gonca.negis@gmail.com>
2024-09-15 16:12:33 +0300

    mkae page grid more responsive

--- components/PageWrapper.vue
+++ components/PageWrapper.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="w-full flex flex-col md:flex-row gap-6">
+  <main class="w-full grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
     <slot />
   </main>
 </template>
