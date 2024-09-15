d6bc3e7f4086ecb1994ae6b359e3e297d6012c72
goncanegis <gonca.negis@gmail.com>
2024-09-15 16:13:03 +0300

    create racetrack wrapper

---
+++ components/RacetrackWrapper.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="race-track flex-1">
+    <h2 class="text-2xl mb-4 font-bold">Racetrack</h2>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.race-track {
+  display: flex;
+  flex-direction: column;
+}
+</style>
