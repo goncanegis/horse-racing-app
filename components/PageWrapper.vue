a2984ece8247ff2f2fd9b2166caac365e8883216
goncanegis <gonca.negis@gmail.com>
2024-09-15 16:37:40 +0300

    row height adjustment to pagewrapper

--- components/PageWrapper.vue
+++ components/PageWrapper.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="w-full grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
+  <main
+    class="w-full grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 auto-rows-min"
+  >
     <slot />
   </main>
 </template>
